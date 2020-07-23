e8288d8c64eda7350b94a2964c0d2aadacd80b12
Ivan Kirov <ivankirov55@gmail.com>
2020-07-23 12:33:15 +0300

    Begin of the project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Green_vs_Red
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
